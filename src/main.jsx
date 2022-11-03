6f59e7a9c0b186ef4ca72eb504229e9457782154
unknown <jvaquiro23@itfip.edu.co>
2022-11-03 11:54:21 -0500

    rutas

--- src/main.jsx
+++ src/main.jsx
@@ -19,12 +19,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <AutoProvider>
       <BrowserRouter>
         <Routes>
-          <Route index element={<App />} />
           <Route path="/" element={<App />} />
           <Route path="login" element={<IniciarSesion />} />
           <Route path="Registrace" element={<Registrace />} />
-        </Routes>
-        <Routes>
           <Route path="Index" element={<IndexContents />} />
           <Route path="IngresarProducto" element={<InsertProduct />} />
           <Route path="IditarProducto/:id" element={<UpdateProduct />} />
